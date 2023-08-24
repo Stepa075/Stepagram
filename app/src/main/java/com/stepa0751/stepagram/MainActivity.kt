3f62c357fa5328c612b43daeae96d95427380e00
Stepa075 <stepa0751@gmail.com>
2023-08-24 16:24:18 +0300

    with driver navigation menu

--- app/src/main/java/com/stepa0751/stepagram/MainActivity.kt
+++ app/src/main/java/com/stepa0751/stepagram/MainActivity.kt
@@ -2,6 +2,8 @@ package com.stepa0751.stepagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.GravityCompat
 import com.stepa0751.stepagram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.apply {
+
+
+            navView.setNavigationItemSelectedListener {
+               when(it.itemId){
+                   R.id.item1 ->{
+                       Toast.makeText(this@MainActivity, "Item 1", Toast.LENGTH_SHORT).show()
+                   }
+                   R.id.item2 -> Toast.makeText(this@MainActivity, "Item 2", Toast.LENGTH_SHORT).show()
+
+               }
+                driwer.closeDrawer(GravityCompat.START)
+                true
+            }
+        }
     }
 }
